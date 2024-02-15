92de5a38e59f6ea23ae0b926b5c6cda4d8ec653d
Matheus Pimentel Pereira da Silva <matheus.silva@take.net>
2024-02-15 19:21:27 -0300

    teste git

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+//instancia jquery e evita confiltos
+jQuery(function($){
+    let titulos = $('h4')// tag
+    let items =  $('.featured-item')//class
+    let feat =  $('#featured')//id
+
+    console.log(titulos.first());
+    
+    
+   
+})
