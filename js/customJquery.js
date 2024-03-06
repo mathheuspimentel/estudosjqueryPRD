da015c3aa707b1b06184a1b747b6dabf0ea49e3f
Matheus Pimentel Pereira da Silva <matheus.silva@take.net>
2024-03-06 19:25:14 -0300

    commitando exemploJquery.html e customJquery.js

---
+++ js/customJquery.js
@@ -0,0 +1,8 @@
+jQuery (function ($)
+    {
+        $('h4') // Seletor para elemento
+        $('.featured-item') //Seletor para Class
+        $('#featured') // Seletor para ID
+        $('div h4') // Tipo de seletor composto
+    }
+)
